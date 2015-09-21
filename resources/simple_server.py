66e905504c40c2c8ef9dd9f9dc638bd991f871b4
unspar <tkamitaki@wesleyan.edu>
2015-09-20 21:43:03 -0400

    simple server added

--- resources/simple_server.py
+++ resources/simple_server.py
@@ -1,22 +1,28 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
 import sys
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+import http.server as sv
 
 
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
+class MyHandeler(sv.BaseHTTPRequestHandler):
+	def do_GET(s):
+		'''this responds to a get request'''
+		
+		s.send_response(200)
+		s.send_header('Content-type', "text/html")
+		s.end_headers()
+		s.wfile.write(b"<html><head><title>Title goes here.</title></head>")
+		s.wfile.write(b"<body><p>This page is dynamically generated</p>")
+		s.wfile.write(b"</body></html>")
 
-if sys.argv[1:]:
-	port = int(sys.argv[1])
-else:
-	port = 80
-	server_address = ('127.0.0.1', port)
-	HandlerClass.protocol_version = Protocol
-	httpd = ServerClass(server_address, HandlerClass)
 
-	sa = httpd.socket.getsockname()
-	print "Serving HTTP on", sa[0], "port", sa[1], "..."
+def run(server_class=sv.HTTPServer, handler_class=MyHandeler):
+	server_address = ('', 8000)
+	httpd = server_class(server_address, handler_class)
 	httpd.serve_forever()
+
+
+
+run()
+
+
